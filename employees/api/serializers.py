e0e80268e77b6d098e6e56beee362e067f278b62
TheRodrigoBraga <rodrigo.bragaaa@gmail.com>
2021-02-22 23:34:53 -0300

    Return

--- employees/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from employees import models
-
-
-class EmployeesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Employees
-        fields = '__all__'
