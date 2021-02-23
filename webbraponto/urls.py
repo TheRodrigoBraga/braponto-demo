d6d6d1b111f8915418dfcf441b7eabf44472e0ff
TheRodrigoBraga <rodrigo.bragaaa@gmail.com>
2021-02-22 22:16:15 -0300

    Adicionada API Employees

--- webbraponto/urls.py
+++ webbraponto/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from employees.api import viewsets as employeesviewsets
+
+route = routers.DefaultRouter()
+
+route.register(r'employees', employeesviewsets.EmployeesViewSet, basename="Employees")
 
 from webbraponto.home_view import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('/api', include(route.urls)),
 ]
